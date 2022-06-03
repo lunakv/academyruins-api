eb140637ed46feaabc0974c304e5c3968dc1a90d
Vaclav Lunak <v.lunak@gmail.com>
2022-06-03 13:27:24 +0200

    add cr scraper

---
+++ app/notifier.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+_uri = 'https://api.pushover.net/1/messages.json'
+_token = os.environ['PUSHOVER_APP_TOKEN']
+_user = os.environ['PUSHOVER_USER_KEY']
+_refresh_uri = None # TODO
+
+def notify(message, title, url, formatted):
+    if os.environ['USE_PUSHOVER'] != '1':
+        return
+    payload = { 'token': _token, 'user': _user, 'message': message }
+    if title: payload['title'] = title
+    if url: payload['url'] = url
+    if formatted: payload['html'] = 1
+
+    requests.post(_uri, data=payload)
+
+def notify_scrape_error(message):
+    notify(message, title='Scraping Error')
+
+def notify_new_cr(link):
+    notify(formatted=True, message=f'New CR version is <a href="{link}">available</a> and ready to be parsed', title='Found new CR', url=_refresh_uri)
