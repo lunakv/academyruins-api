2565fde0f45b589e0495a96813849338e5e8828d
Vaclav Lunak <v.lunak@gmail.com>
2022-08-27 18:33:13 +0200

    coalesce missed jobs, update env example

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -1,7 +1,6 @@
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from . import db
 from .backup import run_backup
 from .logger import logger
 from ..parsing.cr_scraper import scrape_rules_page
@@ -15,7 +14,7 @@ class Scheduler:
 
     def start(self):
         self.scheduler.start()
-        self.scheduler.add_job(scrape_rules_page, "interval", hours=1)
-        self.scheduler.add_job(scrape_docs_page, "interval", hours=1)
-        self.scheduler.add_job(run_backup, "interval", weeks=2)
+        self.scheduler.add_job(scrape_rules_page, "interval", hours=1, coalesce=True)
+        self.scheduler.add_job(scrape_docs_page, "interval", hours=1, coalesce=True)
+        self.scheduler.add_job(run_backup, "interval", weeks=2, coalesce=True)
         logger.info("Started periodic scrape job")
