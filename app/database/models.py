be7ca4182c9beafdafff67b00c81cf772a40486b
Vaclav Lunak <v.lunak@gmail.com>
2022-09-28 11:58:42 +0200

    remove explicit id sequence, add initial migration

--- app/database/models.py
+++ app/database/models.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class Cr(Base):
     __tablename__ = "cr"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('cr_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     creation_day = Column(Date)
     set_code = Column(String(5))
     set_name = Column(String(50))
@@ -19,7 +19,7 @@ class Cr(Base):
 class PendingCr(Base):
     __tablename__ = "cr_pending"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('cr_pending_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     creation_day = Column(Date)
     data = Column(JSONB(astext_type=Text()))
     file_name = Column(Text)
@@ -28,7 +28,7 @@ class PendingCr(Base):
 class Ipg(Base):
     __tablename__ = "ipg"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('table_name_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     creation_day = Column(Date, index=True)
     file_name = Column(Text)
 
@@ -36,7 +36,7 @@ class Ipg(Base):
 class Mtr(Base):
     __tablename__ = "mtr"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('mtr_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     file_name = Column(Text)
     creation_day = Column(Date, index=True)
 
@@ -58,7 +58,7 @@ class PendingRedirect(Base):
 class CrDiff(Base):
     __tablename__ = "cr_diffs"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('cr_diffs_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     creation_day = Column(Date, nullable=False)
     changes = Column(JSONB(astext_type=Text()))
     source_id = Column(ForeignKey("cr.id"), nullable=False)
@@ -72,7 +72,7 @@ class CrDiff(Base):
 class PendingCrDiff(Base):
     __tablename__ = "cr_diffs_pending"
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('cr_diffs_pending_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True)
     creation_day = Column(Date)
     source_id = Column(ForeignKey("cr.id"), nullable=False)
     dest_id = Column(ForeignKey("cr_pending.id", ondelete="CASCADE"), nullable=False)
