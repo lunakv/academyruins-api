d89655c388cc6be3c75bbe7a31f539bae68b7bdc
Vaclav Lunak <v.lunak@gmail.com>
2023-09-03 10:27:57 +0200

    normalize CR diff schema

--- app/utils/trace.py
+++ app/utils/trace.py
@@ -1,74 +1,28 @@
-from sqlalchemy.orm import Session
-
-import app.database.operations as ops
-from app.database.models import CrDiff
+from app.database.models import CrDiffItem, DiffItemKind
 from app.utils.response_models import CRDiffMetadata, TraceDiffRule, TraceItem, TraceItemAction
 
 
-def create_cr_trace(db: Session, rule_id: str) -> list[TraceItem]:
-    trace = []
-    current_diff = ops.get_cr_diff(db, None, None)
-    while current_diff:
-        change = try_get_diff_change(current_diff, rule_id)
-        if change:
-            trace.append(change)
-            if change.action == TraceItemAction.created:
-                break
-            rule_id = change.old.ruleNum
-
-        current_diff = ops.get_cr_diff(db, None, current_diff.source.set_code)
-
-    return trace
-
-
-def try_get_diff_change(diff: CrDiff, rule_id: str) -> TraceItem | None:
-    change = changes_to_trace_item(diff, rule_id) or moves_to_trace_item(diff, rule_id) or None
-    if not change:
-        return None
-
-    return change
-
-
-def moves_to_trace_item(diff: CrDiff, rule_id: str) -> TraceItem | None:
-    matched_move = [m for m in diff.moves if m[1] == rule_id]
-    if not matched_move:
-        return None
-
-    match = matched_move[0]
+def format_trace_item(db_item: CrDiffItem) -> TraceItem:
     return TraceItem(
-        action=TraceItemAction.moved,
-        old=TraceDiffRule(ruleNum=match[0], ruleText=None),
-        new=TraceDiffRule(ruleNum=match[1], ruleText=None),
-        diff=get_trace_diff_metadata(diff),
+        action=get_change_action(db_item),
+        old=TraceDiffRule(ruleNum=db_item.old_number, ruleText=db_item.old_text) if db_item.old_number else None,
+        new=TraceDiffRule(ruleNum=db_item.new_number, ruleText=db_item.new_text),
+        diff=get_trace_diff_metadata(db_item),
     )
 
 
-def changes_to_trace_item(diff: CrDiff, rule_id: str) -> TraceItem | None:
-    matched_change = [c for c in diff.changes if c["new"] and c["new"]["ruleNum"] == rule_id]
-    if not matched_change:
-        return None
-
-    match = matched_change[0]
-    old = TraceDiffRule(**match["old"]) if match.get("old") else None
-    return TraceItem(
-        action=get_change_action(match),
-        old=old,
-        new=TraceDiffRule(ruleNum=match["new"]["ruleNum"], ruleText=match["new"]["ruleText"]),
-        diff=get_trace_diff_metadata(diff),
-    )
-
-
-def get_change_action(change) -> TraceItemAction:
-    if not change.get("old"):
+def get_change_action(change: CrDiffItem) -> TraceItemAction:
+    if change.kind == DiffItemKind.addition:
         return TraceItemAction.created
-    if change["old"]["ruleNum"] == change["new"]["ruleNum"]:
-        return TraceItemAction.edited
-    if not change["old"].get("ruleText") and not change["new"].get("ruleText"):
+    if change.kind == DiffItemKind.move:
         return TraceItemAction.moved
+    if change.old_number == change.new_number:
+        return TraceItemAction.edited
     return TraceItemAction.replaced
 
 
-def get_trace_diff_metadata(diff: CrDiff) -> CRDiffMetadata:
+def get_trace_diff_metadata(diff_item: CrDiffItem) -> CRDiffMetadata:
+    diff = diff_item.diff
     return CRDiffMetadata(
         source_code=diff.source.set_code,
         source_set=diff.source.set_name,
